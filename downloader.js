c7acfde4495b0c7470610bad66684d886c8613ba
Laurent Brand <laurentbrand19@gmail.com>
2021-07-18 13:40:04 +0200

    update

--- downloader.js
+++ downloader.js
@@ -1,33 +1,56 @@
 var YoutubeMp3Downloader = require("youtube-mp3-downloader");
 
-//Configure YoutubeMp3Downloader with your settings
-var YD = new YoutubeMp3Downloader({
-    "ffmpegPath": "C:/Users/laure/ffmpeg/bin/ffmpeg.exe",        // FFmpeg binary location
-    "outputPath": "./podcasts",    // Output file location (default: the home directory)
-    "youtubeVideoQuality": "highestaudio",  // Desired video quality (default: highestaudio)
-    "queueParallelism": 2,                  // Download parallelism (default: 1)
-    "progressTimeout": 2000,                // Interval in ms for the progress reports (default: 1000)
-    "allowWebm": false                      // Enable download from WebM sources (default: false)
-});
-
-//Download video and save as MP3 file
-YD.download("iI34LYmJ1Fs");
-
-YD.on("finished", function(err, data) {
-    console.log(JSON.stringify(data));
-});
-
-YD.on("error", function(error) {
-    console.log(error);
-});
-
-YD.on("progress", function(progress) {
-    console.log(JSON.stringify(progress));
-});
 
 
-function download(videoId) {
-    YD.download(videoId);
+// // //Configure YoutubeMp3Downloader with your settings
+// var YD = new YoutubeMp3Downloader({
+//     "ffmpegPath": "C:/Users/laure/ffmpeg/bin/ffmpeg.exe",        // FFmpeg binary location
+//     "outputPath": "./podcasts",    // Output file location (default: the home directory)
+//     "youtubeVideoQuality": "highestaudio",  // Desired video quality (default: highestaudio)
+//     "queueParallelism": 2,                  // Download parallelism (default: 1)
+//     "progressTimeout": 2000,                // Interval in ms for the progress reports (default: 1000)
+//     "allowWebm": false                      // Enable download from WebM sources (default: false)
+// });
+
+// //Download video and save as MP3 file
+// YD.download("iI34LYmJ1Fs");
+
+
+
+// function download(videoId) {
+//     YD.download(videoId);
+// }
+
+
+class Downloader {
+    constructor() {
+        this.YD = new YoutubeMp3Downloader({
+            "ffmpegPath": "C:/Users/laure/ffmpeg/bin/ffmpeg.exe",        // FFmpeg binary location
+            "outputPath": "./podcasts",    // Output file location (default: the home directory)
+            "youtubeVideoQuality": "highestaudio",  // Desired video quality (default: highestaudio)
+            "queueParallelism": 2,                  // Download parallelism (default: 1)
+            "progressTimeout": 2000,                // Interval in ms for the progress reports (default: 1000)
+            "allowWebm": false                      // Enable download from WebM sources (default: false)
+        });
+    }
+
+    download(videoId) {
+        this.YD.download(videoId);
+        this.YD.on("finished", function(err, data) {
+            console.log(JSON.stringify(data));
+        });
+        
+        this.YD.on("error", function(error) {
+            console.log(error);
+        });
+        
+        this.YD.on("progress", function(progress) {
+            console.log(JSON.stringify(progress));
+        });
+    }
+
+    
+    
 }
 
-
+module.exports = Downloader;
--- youtube.js
+++ youtube.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const { google } = require('googleapis')
 const youtube = google.youtube('v3')
 
+const Downloader = require('./downloader.js')
+
 function getPlaylistId(channelId) {
     return new Promise((resolve, reject) => {
         youtube.channels.list({
@@ -30,6 +32,9 @@ function getVideosFromPlaylistId(playlistId){
         }).then(response => {
             const videos = response.data.items
             resolve(response.data.items)
+        }).catch(error => {
+            console.log(error)
+            reject(error)
         })
     })
 }
@@ -54,71 +59,36 @@ function getLastVideoIdFrom(channelId) {
     return channel.lastVideoId
 }
 
-async function isVideoIdNew(channelId, videoId) {
-    channel = await getChannel(channelId)
-    return new Promise((resolve, reject) => {
-        getPlaylistId(channelId).then(async playlistId => {
-            videos = await getVideosFromPlaylistId(playlistId).then(videos => {
-                lastVideoId = getLastVideoIdFrom(channelId)
-                if (lastVideoId === videoId) {
-                    resolve(false) 
-                } else {
-                    resolve(true)
-                }
+function updateChannelData(channel) {
+    channels_json = getAllChannels()
+    for(i = 0; i < channels_json.channels.length; i++){
+        if(channels_json.channels[i].id == channel.id){
+            channels_json.channels[i] = channel
+        }
+    }
+    fs.writeFileSync('./channels/channels.json', JSON.stringify(channels_json, null, 2))
+}
+
+function run() {
+    channels = getAllChannels().channels
+    channels.forEach(async channel => {
+        channelId = channel.id
+        getPlaylistId(channelId).then(playlistId => {
+            getVideosFromPlaylistId(playlistId).then(videos => {
+                videos.forEach(video => {
+                    videoId = video.snippet.resourceId.videoId
+                    console.log("VideoId", videoId , " Channel:", channel.name )
+                    if (channel.lastVideoId != videoId){
+                        console.log("New VideoId", videoId , " Channel:", channel.name )
+                        channel.lastVideoId = videoId
+                        updateChannelData(channel)
+                        var downloader = new Downloader()                        
+                        downloader.download(videoId)
+                    }
+                })
             })
         })
     })
 }
 
-channels = getAllChannels().channels
-
-async function run() {
-
-    for(i = 0; i < channels.length; i++) {
-        console.log("i", i)
-        channelId = channels[i].id
-        await getPlaylistId(channelId).then(async playlistId => {
-            await getVideosFromPlaylistId(playlistId).then(async videos => {
-                for(j = 0; j < videos.length; j++){
-                    
-                    videoId = videos[j].snippet.resourceId.videoId
-                    console.log("VideoId", videoId)
-                    await isVideoIdNew(channelId, videoId).then(isNew => {
-                        if(isNew){
-                            console.log("ChannelId in isNew : ", channelId)
-                            setLastVideoIdOf(channelId, videoId)
-                        }
-                    })
-                }
-            })
-        })
-    }
-}
-
 run()
-
-// temp = isVideoIdNew('UCesjlAoEgN_Sz_cKTvKEmmw', 'jimmy').then(async isNew => {
-//     if (isNew) {
-//         console.log("New Video")
-//         setLastVideoIdOf('UCesjlAoEgN_Sz_cKTvKEmmw', 'jimmy')
-//     } else {
-//         console.log("Video already found")
-//     }
-// })
-
-function setLastVideoIdOf(channelId, videoId){
-    channel = getChannel(channelId)
-    channel.lastVideoId = videoId
-
-    channels_json = getAllChannels()
-
-    for(i = 0; i < channels_json.channels.length; i++){
-        console.log(channels_json.channels[i].name , " channelId", channelId , " channelId:json", channels_json.channels[i].id)
-        if(channels_json.channels[i].id == channelId){
-            
-            channels_json.channels[i].lastVideoId = videoId
-        }
-    }
-
-    fs.writeFileSync('./channels/channels.json', JSON.stringify(channels_json, null, 2))
-}
