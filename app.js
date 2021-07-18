da4b772d876c2d7236e873321f6315e4dc19c121
Laurent Brand <laurentbrand19@gmail.com>
2021-07-18 23:55:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,16 +13,12 @@ app.get('/', (req, res) => {
             res.send(err);
         } else {
             obj = JSON.parse(data);
-            console.log(obj)
             res.header("Content-Type",'application/json');
             res.send(JSON.stringify(obj, null, 4))
         }
     })
-   
-    
 })
 
 app.listen(3000, () => {
     console.log(`Server is running at http://localhost:3000`)
-
 })
